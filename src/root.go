88f34ea7166ad562f8f039307504a1df7690959f
Oleksiy <oomelche@ucsc.edu>
2021-04-10 22:00:36 -0700

    finished get & delete requests (for now)

--- src/root.go
+++ src/root.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"utility"
 
 	"github.com/gin-gonic/gin"
@@ -16,11 +15,10 @@ func main() {
 	router := gin.Default()
 
 	var dict = make(map[string]string) // key-value store for PUT, GET, & DELETE requests (exported variable)
-	// fmt.Println(router, Dict)
+
 	utility.PutRequest(router, dict)
-	for key, value := range dict {
-		fmt.Printf("key %s, value %s\n", key, value)
-	}
+	utility.GetRequest(router, dict)
+	utility.DeleteRequest(router, dict)
 
 	router.Run(port)
 }
