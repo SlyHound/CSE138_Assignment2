428adeb61cdc5f5d2d816ca3eed9b254f8239a60
Oleksiy <oomelche@ucsc.edu>
2021-04-14 18:14:01 -0700

    begin work on forwarding

--- src/root.go
+++ src/root.go
@@ -17,21 +17,17 @@ func main() {
 	router := gin.Default()
 
 	var dict = make(map[string]string) // key-value store for PUT, GET, & DELETE requests (exported variable)
-
-	fmt.Println(os.Getenv("FORWARDING_ADDRESS"))
+	// fmt.Println(os.Getenv("FORWARDING_ADDRESS"))
 
 	// if there is a forwarding address, then we the forwarding instance is running & //
 	// therefore we can forward requests to the main instance //
 	if os.Getenv("FORWARDING_ADDRESS") != "" {
-		fmt.Println("in if check")
-		utility.ForwardRequest()
-
+		utility.ForwardRequest(router)
 	} else { // otherwise we can directly handle requests as we're the main instance //
 		fmt.Println("in else")
 		utility.PutRequest(router, dict)
 		utility.GetRequest(router, dict)
 		utility.DeleteRequest(router, dict)
-
 	}
 	err := router.Run(port)
 	if err != nil {
--- src/utility/forward.go
+++ src/utility/forward.go
@@ -1,7 +1,6 @@
 package utility
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -13,33 +12,64 @@ type JSON struct {
 	Response string `json:"message"`
 }
 
-func ForwardRequest() gin.HandlerFunc {
+func ForwardRequest(r *gin.Engine) {
+	fmt.Println("In ForwardRequest call")
+	r.GET("/key-value-store/:key", handleRequests())
+	r.PUT("/key-value-store/:key", handleRequests())
+	r.DELETE("/key-value-store/:key", handleRequests())
+	// fwdRequest, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
+	// fmt.Println("fwdRequest method(0):", fwdRequest.Method)
+	// if err != nil {
+	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
+	// 	return
+	// }
+
+	// fwdRequest.Header.Set("X-Forwarding-For", r.RemoteAddr)
+	// fmt.Println("fwdRequest method:", fwdRequest.Method)
+
+	// httpForwarder := &http.Client{}
+	// fwdResponse, err := httpForwarder.Do(fwdRequest)
+	// fmt.Println("fwdResponse method:", fwdResponse.Request.Method)
+	// if err != nil {
+	// 	w.WriteHeader(http.StatusServiceUnavailable) // sends a 503 status code
+	// 	response := JSON{
+	// 		Error:    "Main instance is Down",
+	// 		Response: "Error in " + fwdResponse.Request.Method,
+	// 	}
+	// 	json.NewEncoder(w).Encode(response)
+	// }
+
+	// fwdResponse.Body.Close()
+}
+
+func handleRequests() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		fmt.Println("In ForwardRequest call")
-		http.HandleFunc("/key-value-store/{key}", func(w http.ResponseWriter, r *http.Request) {
-			fwdRequest, err := http.NewRequest(r.Method, r.URL.String(), r.Body)
-			fmt.Println("fwdRequest method(0):", fwdRequest.Method)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
+		c.Request.URL.Host = "10.10.0.2:8085"
+		c.Request.URL.Scheme = "http"
 
-			fwdRequest.Header.Set("X-Forwarding-For", r.RemoteAddr)
-			fmt.Println("fwdRequest method:", fwdRequest.Method)
+		fmt.Println("method & url:", c.Request.Method, c.Request.URL.String())
+		fwdRequest, err := http.NewRequest(c.Request.Method, c.Request.URL.String(), c.Request.Body)
+		fmt.Println("finished getting fwdRqst")
+		if err != nil {
+			http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
-			httpForwarder := &http.Client{}
-			fwdResponse, err := httpForwarder.Do(fwdRequest)
-			fmt.Println("fwdResponse method:", fwdResponse.Request.Method)
-			if err != nil {
-				w.WriteHeader(http.StatusServiceUnavailable) // sends a 503 status code
-				response := JSON{
-					Error:    "Main instance is Down",
-					Response: "Error in " + fwdResponse.Request.Method,
-				}
-				json.NewEncoder(w).Encode(response)
-			}
+		fwdRequest.Header = c.Request.Header
+		// fwdRequest.Header.Set("Host", "127.0.0.1:8086")
+		// for debugging purposes //
+		fmt.Println(fwdRequest.Header)
 
-			fwdResponse.Body.Close()
-		})
+		httpForwarder := &http.Client{}
+		fwdResponse, err := httpForwarder.Do(fwdRequest)
+		fmt.Println("finished getting fwdResponse")
+		if err != nil {
+			msg := "Error in " + fwdRequest.Method
+			c.JSON(http.StatusServiceUnavailable, gin.H{"error": "Main instance is down", "message": msg})
+		}
+		if fwdResponse != nil {
+			c.JSON(fwdResponse.StatusCode, fwdResponse.Body)
+			defer fwdResponse.Body.Close()
+		}
 	}
 }
